fd2f257146e3c058f03fe784aea1dc823baa19dd
Wannes Vanderlinden <wannesvanderlindenstudent.ehb.be>
2021-11-13 11:58:44 +0100

    update login and reg

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,14 +7,42 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Login</h1>
-       <form action="" method="post">
-Email:<textarea name="email" id="email" cols="30" rows="1"></textarea>
-<br>
-Password: <input type="password" name="password" id="password">
-<br>
-@csrf
-<button type="submit">Log in</button>
-    </form>
+   @extends('layouts\user')
+
+@section('content')
+
+<body>
+    <div id="login">
+        <h3 class="text-center text-white pt-5">Login form</h3>
+        <div class="container">
+            <div id="login-row" class="row justify-content-center align-items-center">
+                <div id="login-column" class="col-md-6">
+                    <div id="login-box" class="col-md-12">
+                        <form id="login-form" class="form" action="" method="post">
+                            <h3 class="text-center text-info">Login</h3>
+                            <div class="form-group">
+                                <label for="username" class="text-info">Email:</label><br>
+                                <input type="text" name="email" id="email" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="text-info">Password:</label><br>
+                                <input type="text" name="password" id="password" class="form-control">
+                            </div>
+                            <div class="form-group">
+                            @csrf
+                                <label for="remember-me" class="text-info"><span>Remember me</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
+                                <input type="submit" name="submit" class="btn btn-info btn-md" value="submit">
+                            </div>
+                            <div id="register-link" class="text-right">
+                            
+                                <a href="#" class="text-info">Register here</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+@endsection
 </html>
