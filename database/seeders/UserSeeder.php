50503da16b222979d89b69ef8c61fe19dffeb055
Wannes Vanderlinden <wannesvanderlindenstudent.ehb.be>
2021-11-15 18:16:02 +0100

    seeder for user done

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +15,21 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+       
+        DB::table('users')->insert([
+            'id'=>1,
+             'name' => 'Felix',
+            'username' => 'admin',
+            'email' => 'admin@ehb.be',
+            'password' => Hash::make('Password!321'),
+              'name' => 'Felix',
+                'birthday' => date("Y/m/d"),
+                'aboutMe' => "none",
+                'admin'=>true,
+                'created_at'=>date("Y/m/d"),
+                'updated_at'=>date("Y/m/d"),
+                'remember_token'=>null
+                
+        ]);
     }
 }
